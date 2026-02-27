27e97700fb7a9ad6aa840ba6634764c32644637a
sinansevg1 <9a.sinansevgi@gmail.com>
2026-02-27 18:52:11 +0300

    Web UI tarafında bildirimler için CRUD altyapısı eklendi. NotificationsController oluşturuldu, Create/Update/Result DTO sınıfları tanımlandı, Index sayfası eklendi

--- SignalRApi/Controllers/NotificationController.cs
+++ SignalRApi/Controllers/NotificationController.cs
@@ -46,7 +46,7 @@ namespace SignalRApi.Controllers
             _notificationService.TAdd(notification);
             return Ok("Ekleme işlemi başarıyla yapıldı");
         }
-        [HttpDelete]
+        [HttpDelete("{id}")]
         public IActionResult DeleteNotification(int id)
         {
             var value = _notificationService.TGetByID(id);
