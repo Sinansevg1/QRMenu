1b3705813d26368b5588fab75ee12367c6ae120f
sinansevg1 <9a.sinansevgi@gmail.com>
2026-03-05 02:12:29 +0300

    Anlık kullanıcı sayısı görüntüleme işlemleri için gerekli metorlar ve görüntüleme sayfası yazıldı.

--- SignalRApi/Hubs/SignalRHub.cs
+++ SignalRApi/Hubs/SignalRHub.cs
@@ -23,7 +23,7 @@ namespace SignalRApi.Hubs
             _bookingService = bookingService;
             _notificationService = notificationService;
         }
-
+        public static int clientCount { get; set; } = 0;
         public async Task SendStatistic()
         {
             var value = _categoryService.TCategoryCount();
@@ -100,9 +100,21 @@ namespace SignalRApi.Hubs
             var value = _menuTableService.TGetListAll();
             await Clients.All.SendAsync("ReceiveMenuTableStatus", value);
         }
-        public async Task SendMessage(string user,string message)
+        public async Task SendMessage(string user, string message)
         {
             await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+        public override async Task OnConnectedAsync()
+        {
+            clientCount++;
+            await Clients.All.SendAsync("ReceiveClientCount", clientCount);
+            await base.OnConnectedAsync();
+        }
+        public override async Task OnDisconnectedAsync(Exception? exception)
+        {
+            clientCount--;
+            await Clients.All.SendAsync("ReceiveClientCount", clientCount);
+            await base.OnDisconnectedAsync(exception);
         }
     }
 }
