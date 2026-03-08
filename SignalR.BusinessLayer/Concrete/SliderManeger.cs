7a219471b5439d5a2b072c87a4b635310c2838e2
sinansevg1 <9a.sinansevgi@gmail.com>
2026-03-09 02:54:12 +0300

    Slider alanının backend tarafındaki revizeleri yapıldı.

--- SignalR.BusinessLayer/Concrete/SliderManeger.cs
+++ SignalR.BusinessLayer/Concrete/SliderManeger.cs
@@ -20,17 +20,16 @@ namespace SignalR.BusinessLayer.Concrete
 
         public void TAdd(Slider entity)
         {
-            throw new NotImplementedException();
+          _sliderDal.Add(entity);
         }
-
         public void TDelete(Slider entity)
         {
-            throw new NotImplementedException();
+            _sliderDal.Delete(entity);
         }
 
         public Slider TGetByID(int id)
         {
-            throw new NotImplementedException();
+           return _sliderDal.GetByID(id);
         }
 
         public List<Slider> TGetListAll()
@@ -40,7 +39,7 @@ namespace SignalR.BusinessLayer.Concrete
 
         public void TUpdate(Slider entity)
         {
-            throw new NotImplementedException();
+            _sliderDal.Update(entity);
         }
     }
 }
