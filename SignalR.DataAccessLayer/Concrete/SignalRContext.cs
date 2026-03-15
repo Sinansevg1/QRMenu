a9ef758c10ef6a661f7ec5e26e8be0df098497db
sinansevg1 <9a.sinansevgi@gmail.com>
2026-03-15 03:52:26 +0300

    Messajlar için entity yazıldı ve migration işlemi başarı ile yapıldı

--- SignalR.DataAccessLayer/Concrete/SignalRContext.cs
+++ SignalR.DataAccessLayer/Concrete/SignalRContext.cs
@@ -33,6 +33,8 @@ namespace SignalR.DataAccessLayer.concrete
         public DbSet<Slider>Sliders { get; set; }
         public DbSet<Basket> Baskets { get; set; }
         public DbSet<Notification> Notifications { get; set; }
+        public DbSet<Message> Messages { get; set; }
+
 
         }
 }
