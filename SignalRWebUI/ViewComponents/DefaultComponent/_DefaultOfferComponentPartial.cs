4408aaf645482d3953b554036f17b21b009b726a
sinansevg1 <9a.sinansevgi@gmail.com>
2026-03-15 03:31:23 +0300

    indrimler kısımın revizeleri tamamlandı

--- SignalRWebUI/ViewComponents/DefaultComponent/_DefaultOfferComponentPartial.cs
+++ SignalRWebUI/ViewComponents/DefaultComponent/_DefaultOfferComponentPartial.cs
@@ -15,7 +15,7 @@ namespace SignalRWebUI.ViewComponents.DefaultComponent
         public async Task<IViewComponentResult> InvokeAsync()
         {
             var client = _httpClientFactory.CreateClient();
-            var responseMessage = await client.GetAsync("https://localhost:7096/api/Discount");
+            var responseMessage = await client.GetAsync("https://localhost:7096/api/Discount/GetListByStatusTrue");
             var jsonData = await responseMessage.Content.ReadAsStringAsync();
             var values = JsonConvert.DeserializeObject<List<ResultDiscountDto>>(jsonData);
             return View(values);
