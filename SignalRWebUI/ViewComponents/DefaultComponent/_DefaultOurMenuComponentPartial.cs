a647fdc7fb2bac374d7b887c045ea545888f8f6e
sinansevg1 <9a.sinansevgi@gmail.com>
2026-04-29 21:45:34 +0300

    ana sayfada 9 ürün listelenme işlemi tamamlandı

--- SignalRWebUI/ViewComponents/DefaultComponent/_DefaultOurMenuComponentPartial.cs
+++ SignalRWebUI/ViewComponents/DefaultComponent/_DefaultOurMenuComponentPartial.cs
@@ -14,7 +14,7 @@ namespace SignalRWebUI.ViewComponents.DefaultComponent
         public async Task<IViewComponentResult> InvokeAsync()
         {
             var client = _httpClientFactory.CreateClient();
-            var responseMessage = await client.GetAsync("https://localhost:7096/api/Product");
+            var responseMessage = await client.GetAsync("https://localhost:7096/api/Product/GetLast9Productd\r\n");
             var jsonData = await responseMessage.Content.ReadAsStringAsync();
             var values = JsonConvert.DeserializeObject<List<ResultProductDto>>(jsonData);
             return View(values);
