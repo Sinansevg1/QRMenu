9d86298a8e78ce55157259e858fce8697ffa9eb3
sinansevg1 <9a.sinansevgi@gmail.com>
2026-05-01 21:14:39 +0300

    menü seçeneğine göre ürün getirme işlemi tamamlandı

--- SignalRWebUI/Controllers/MenuController.cs
+++ SignalRWebUI/Controllers/MenuController.cs
@@ -16,7 +16,7 @@ namespace SignalRWebUI.Controllers
         public async Task<IActionResult> Index()
         {
             var client = _httpClientFactory.CreateClient();
-            var responseMessage = await client.GetAsync("https://localhost:7096/api/Product");
+            var responseMessage = await client.GetAsync("https://localhost:7096/api/Product/ProductListWithCategory\r\n");
             var jsonData = await responseMessage.Content.ReadAsStringAsync();
             var values = JsonConvert.DeserializeObject<List<ResultProductDto>>(jsonData);
             return View(values);
