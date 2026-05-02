0f416b113301342372c8c7617770044a617ad193
sinansevg1 <9a.sinansevgi@gmail.com>
2026-05-02 19:17:26 +0300

    rapid api üzerinden kodların çekilmesi işlemi yapıldı gerekli UI sayfası , controller ve dtos  yazıldı

---
+++ SignalRWebUI/Dtos/RapidApiDtos/ResultTastyApi.cs
@@ -0,0 +1,11 @@
+﻿namespace SignalRWebUI.Dtos.RapidApiDtos
+{
+    public class ResultTastyApi
+    {
+        public string Name { get; set; }
+        public string original_video_url { get; set; }
+        public int total_time_minutes { get; set; }
+        public string thumbnail_url { get; set; }
+       
+    }
+}
