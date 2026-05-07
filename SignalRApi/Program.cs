6e84e10d7e97187f0f3722400d1ab69ec46d64fa
sinansevg1 <9a.sinansevgi@gmail.com>
2026-05-07 14:03:48 +0300

    extension sınıfı yazıldı

--- SignalRApi/Program.cs
+++ SignalRApi/Program.cs
@@ -1,6 +1,7 @@
 using FluentValidation;
 using SignalR.BusinessLayer.Abstract;
 using SignalR.BusinessLayer.Concrete;
+using SignalR.BusinessLayer.Container;
 using SignalR.BusinessLayer.ValidationRules.BookingValidations;
 using SignalR.DataAccessLayer.Abstract;
 using SignalR.DataAccessLayer.concrete;
@@ -26,56 +27,7 @@ builder.Services.AddSignalR();
 builder.Services.AddDbContext<SignalRContext>();
 builder.Services.AddAutoMapper(cfg => { }, Assembly.GetExecutingAssembly());
 
-builder.Services.AddScoped<IAboutService, AboutManeger>();
-builder.Services.AddScoped<IAboutDal, EfAboutDal>();
-
-builder.Services.AddScoped<IBookingService, BookingManeger>();
-builder.Services.AddScoped<IBookingDal, EfBookingDal>();
-
-builder.Services.AddScoped<ICategoryService, CategoryManeger>();
-builder.Services.AddScoped<ICategoryDal, EfCategoryDal>();
-
-builder.Services.AddScoped<IContactService, ContactManeger>();
-builder.Services.AddScoped<IContactDal, EfContactDal>();
-
-builder.Services.AddScoped<IDiscountService, DiscountManeger>();
-builder.Services.AddScoped<IDiscountDal, EfDiscountDal>();
-
-builder.Services.AddScoped<IFeatureService, FeatureManeger>();
-builder.Services.AddScoped<IFeatureDal, EfFeatureDal>();
-
-builder.Services.AddScoped<IProductService, ProductManeger>();
-builder.Services.AddScoped<IProductDal, EfProductDal>();
-
-builder.Services.AddScoped<ISocialMediaService, SocialMediaManeger>();
-builder.Services.AddScoped<ISocialMediaDal, EfSocialMediaDal>();
-
-builder.Services.AddScoped<ITestimonialService, TestimonialManeger>();
-builder.Services.AddScoped<ITestimonialDal, EfTestimonialDal>();
-
-builder.Services.AddScoped<IOrderService, OrderManeger>();
-builder.Services.AddScoped<IOrderDal, EfOrderDal>();
-
-builder.Services.AddScoped<IOrderDeteilService, OrderDeteilManeger>();
-builder.Services.AddScoped<IOrderDeteilDal, EfOrderDeteilDal>();
-
-builder.Services.AddScoped<IMoneyCaseService, MoneyCaseManeger>();
-builder.Services.AddScoped<IMoneyCaseDal, EfMoneyCaseDal>();
-
-builder.Services.AddScoped<IMenuTableService, MenuTableManeger>();
-builder.Services.AddScoped<IMenuTableDal, EfMenuTableDal>();
-
-builder.Services.AddScoped<ISliderService, SliderManeger>();
-builder.Services.AddScoped<ISliderDal, EfSliderDal>();
-
-builder.Services.AddScoped<IBasketService, BasketManeger>();
-builder.Services.AddScoped<IBasketDal, EfBasketDal>();
-
-builder.Services.AddScoped<INotificationService, NotificationManeger>();
-builder.Services.AddScoped<INotificationDal, EfNotificationDal>();
-
-builder.Services.AddScoped<IMessageService, MessageMeneger>();
-builder.Services.AddScoped<IMessageDal, EfMessageDal>();
+builder.Services.ContainerDependencies();
 
 builder.Services.AddValidatorsFromAssemblyContaining<CreateBookingValidation>();
 
@@ -96,6 +48,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseCors("CorsPolicy");
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
